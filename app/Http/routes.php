f45ef9f0aba935041c7ad284b3329678f3f5712d
fantasyczl <czilong_0618@126.com>
2015-10-22 01:42:13 +0800

    add login route and view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/auth/login');
 });
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
