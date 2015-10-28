6a5e703de93c937080626d1982a737507aeffbc0
fantasyczl <czilong_0618@126.com>
2015-10-28 21:29:53 +0800

    add reset password function

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,32 +5,30 @@
 @stop
 
 @section ('css')
+    <link rel="stylesheet" href="/css/login.css" type="text/css">
     <style>
-    .form {
-        margin-top: 150px;
-    }
     </style>
 @stop
 
 @section ('body')
     <div class="content-fluid">
 
-        @include ('layouts.messages')
-
-        <form method="POST" action="/auth/login" class="form-horizontal form">
+        <form method="POST" action="/auth/login" class="form-horizontal">
             {!! csrf_field() !!}
+
+            @include ('layouts.messages')
 
             <div class="form-group">
                 <label class="col-xs-3 control-label" for="">邮箱</label>
 
-                <div class="col-xs-3">
+                <div class="col-xs-9">
                     <input type="email" name="email" value="{{ old('email') }}" class="form-control">
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-xs-3 control-label" for="">密码</label>
-                <div class="col-xs-3">
+                <div class="col-xs-9">
                     <input type="password" name="password" id="password" class="form-control">
                 </div>
             </div>
@@ -52,8 +50,12 @@
             </div>
 
             <div class="form-group">
-                <div class="col-xs-offset-3 col-xs-9">
+                <div class="col-xs-offset-3 col-xs-5">
                     <a href="/auth/register">注册</a>
+                </div>
+
+                <div class="col-xs-4 col-md-4">
+                    <a href="/password/email">忘记密码</a>
                 </div>
             </div>
         </form>
---
+++ resources/views/auth/password.blade.php
@@ -0,0 +1,41 @@
+@extends ('layouts.base')
+
+
+@section ('title')
+
+@stop
+
+@section ('css')
+    <link rel="stylesheet" href="/css/login.css" type="text/css">
+    <style>
+    </style>
+@stop
+
+
+@section ('body')
+    <div class="container">
+
+        <form method="POST" action="/password/email" class="form-horizontal">
+            {!! csrf_field() !!}
+
+            @include ('layouts.messages')
+
+            <div class="form-group">
+                <label class="col-xs-3 control-label" for="">邮箱</label>
+
+                <div class="col-xs-9">
+                    <input type="email" name="email" value="{{ old('email') }}" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-xs-offset-3 col-xs-9">
+                    <button type="submit" class="btn btn-success">
+                        Send Password Reset Link
+                    </button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+@stop
