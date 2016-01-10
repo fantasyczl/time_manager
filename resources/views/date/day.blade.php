48ebf67ab5bc8e353bb6e10afd70c4142e8bb6d3
fantasyczl <czilong_0618@126.com>
2016-01-11 00:21:51 +0800

    fixed bug

--- resources/views/date/day.blade.php
+++ resources/views/date/day.blade.php
@@ -14,7 +14,7 @@
 
 @section ('content')
     <div class="row">
-        <div class="col-xs-4" >
+        <div class="col-xs-12" >
             <h2 style="text-align: center">{{ $date }}</h2>
         </div>
     </div>
